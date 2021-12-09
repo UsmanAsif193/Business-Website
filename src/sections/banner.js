417b9e8cfda505e9faeeac3d4bc9bcdb6717881c
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2021-12-09 22:45:49 +0500

    Banner section completed

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -4,10 +4,27 @@ import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
 import BannerImg from 'assets/banner-thumb.png';
 import ShapeLeft from 'assets/shape-left.png';
 import ShapeRight from 'assets/shape-right.png';
+import { style } from "styled-system";
 
 export default function Banner() {
   return (
-    <h1>Banner</h1>
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as="h1" variant="heroPrimary">
+            Top Quality Digital Products to Explore
+          </Heading>
+          <Text as="p" variant="heroSecondary">
+            Check our website to find great software products and deals! If you
+            need a website or a web application this is the place to go!
+          </Text>
+          <Button variant="primary">Explore</Button>
+        </Box>
+        <Box sx={styles.banner.imageBox}>
+          <Image src={BannerImg} alt="Banner" />
+        </Box>
+      </Container>
+    </section>
   );
 }
 
